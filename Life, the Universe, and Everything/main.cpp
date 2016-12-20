7ead7913092df691b54b24a9a8786f53978f5498
Rezwan <r.haque.249.rh@gmail.com>
2016-12-20 19:24:09 +0600

    Add hoc

---
+++ Life, the Universe, and Everything/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    while(x!= 42) {
+        cout<<x<<endl;
+        cin>>x;
+    }
+    return 0;
+}
+
