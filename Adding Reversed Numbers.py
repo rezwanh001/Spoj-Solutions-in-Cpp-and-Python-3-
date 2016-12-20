b2bef92cb2379eba89bd11f827b2ce8e23c99610
Rezwan <r.haque.249.rh@gmail.com>
2016-12-20 19:20:58 +0600

    Add hoc

---
+++ Adding Reversed Numbers.py
@@ -0,0 +1,17 @@
+# your code goes here
+
+'''
+Created on May 20, 2016
+
+@author: Md. Rezwanul Haque
+'''
+n = int(input())
+ans = 0
+rezwan = 0
+for i in range(n):
+    a, b = map(int, input().split())
+    A = int(str(a)[::-1])  # reverse
+    B = int(str(b)[::-1])  # reverse
+    ans = A + B
+    rezwan = int(str(ans)[::-1])  # reverse
+    print(rezwan)
